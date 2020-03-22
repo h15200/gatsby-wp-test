32d95dba682208a7305b734a48c4b5940f886f6f
h15200 <45977354+h15200@users.noreply.github.com>
2020-03-21 23:39:47 -0700

    use contentful instead of wp

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,11 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: 'Gatsby with md files and wp cms',
+    description: "Practice connecting gatsby to a new cms",
+    author: "Hideaki Aomori"
+  },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -14,6 +19,14 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
+
   ]
 }
